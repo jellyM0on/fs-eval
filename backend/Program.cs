19f83c79183da05ecd7d888e009c760754d37d5d
jellyM0on <ylana.ong@gmail.com>
2025-10-31 03:33:56 +0800

    Update cors config

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,7 +19,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     }); 
 });
 
